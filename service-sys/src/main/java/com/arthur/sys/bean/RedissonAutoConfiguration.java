2aa40bd84a9949c3c88d488a0ffa15fb157c4a5b
qiushi <qiushi@baiwang.com>
2018-05-21 15:10:00 +0800

    redisson锁

---
+++ service-sys/src/main/java/com/arthur/sys/bean/RedissonAutoConfiguration.java
@@ -0,0 +1,70 @@
+package com.arthur.sys.bean;
+
+import org.apache.commons.lang.StringUtils;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.redisson.config.SentinelServersConfig;
+import org.redisson.config.SingleServerConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.arthur.sys.properties.RedissonProperties;
+@Configuration
+@ConditionalOnClass(Config.class)
+public class RedissonAutoConfiguration {
+
+  @Autowired
+  private RedissonProperties redssionProperties;
+
+  /**
+   * 哨兵模式自动装配
+   * 
+   * @return
+   */
+  @Bean
+  @ConditionalOnProperty(name = "master-name",prefix="spring.redisson.")
+  RedissonClient redissonSentinel() {
+    System.out.println("master-name created");
+    Config config = new Config();
+    SentinelServersConfig serverConfig =
+        config.useSentinelServers().addSentinelAddress(redssionProperties.getSentinelAddresses())
+            .setMasterName(redssionProperties.getMasterName())
+            .setTimeout(redssionProperties.getTimeout())
+            .setMasterConnectionPoolSize(redssionProperties.getMasterConnectionPoolSize())
+            .setSlaveConnectionPoolSize(redssionProperties.getSlaveConnectionPoolSize());
+
+    if (StringUtils.isNotBlank(redssionProperties.getPassword())) {
+      serverConfig.setPassword(redssionProperties.getPassword());
+    }
+    return Redisson.create(config);
+  }
+
+  /**
+   * 单机模式自动装配
+   * 
+   * @return
+   */
+  @Bean
+  @ConditionalOnProperty(name = "address",prefix="spring.redisson.")
+  RedissonClient redissonSingle() {
+    System.out.println("single created");
+    Config config = new Config();
+    SingleServerConfig serverConfig = config.useSingleServer()
+        .setAddress(redssionProperties.getAddress()).setTimeout(redssionProperties.getTimeout())
+        .setConnectionPoolSize(redssionProperties.getConnectionPoolSize())
+        .setConnectionMinimumIdleSize(redssionProperties.getConnectionMinimumIdleSize());
+
+    if (StringUtils.isNotBlank(redssionProperties.getPassword())) {
+      serverConfig.setPassword(redssionProperties.getPassword());
+    }
+
+    return Redisson.create(config);
+  }
+
+
+
+}
